2dfe997af71545fe7862f928e945fb322c69b7a3
danggeuni <dydrnxkeh@naver.com>
2024-04-26 11:14:34 +0900

    find all user testcode

--- src/test/java/com/user/createUser/controller/UserApiControllerTest.java
+++ src/test/java/com/user/createUser/controller/UserApiControllerTest.java
@@ -1,85 +1,57 @@
 package com.user.createUser.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.user.createUser.controller.dto.RegisterUserRequestDto;
 import com.user.createUser.domain.entity.UserEntity;
 import com.user.createUser.domain.repository.UserRepository;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class UserApiControllerTest {
 
     @Autowired
-    protected MockMvc mockMvc;
-
-    @Autowired
-    protected ObjectMapper objectMapper;
-
-    @Autowired
-    private WebApplicationContext context;
-
-    @Autowired
     UserRepository userRepository;
-
-    @BeforeEach
-    public void setMockMvc() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-        userRepository.deleteAll();
-    }
 
     //    join user test
     @Test
     @DisplayName("joinUser : 회원 가입 성공")
     public void joinUser() throws Exception {
-
         // given
-        final String url = "/api/user/join";
-        final String password = "010203abc";
-        final String nickname = "j";
-        final String name = "jungjunho";
-        final String phone = "010-1234-5678";
-        final String email = "juno@naver.com";
-        final RegisterUserRequestDto registerUserRequestDto = new RegisterUserRequestDto(password, nickname, name, phone, email);
-        final String requestBody = objectMapper.writeValueAsString(registerUserRequestDto);
+        UserEntity entity = new UserEntity(1L, "password", "nickname", "name", "010-1234-5678", "1@naver.com", LocalDateTime.now());
 
         // when
-        ResultActions result = mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(requestBody));
+        userRepository.save(entity);
 
         // then
-        result.andExpect(status().isCreated());
+        Optional<UserEntity> result = userRepository.findById(1L);
 
-        Optional<UserEntity> user = userRepository.findById(1L);
+        assertThat(result.isPresent());
+        assertThat(result.get().getEmail()).isEqualTo(entity.getEmail());
+    }
 
-        // 존재 여부 확인
-        assertThat(user).isPresent();
-        // value 동일 여부 확인
-        assertThat(user.get().getPassword()).isEqualTo(password);
-        assertThat(user.get().getNickname()).isEqualTo(nickname);
-        assertThat(user.get().getName()).isEqualTo(name);
-        assertThat(user.get().getPhone()).isEqualTo(phone);
-        assertThat(user.get().getEmail()).isEqualTo(email);
+    @Test
+    @DisplayName("selectAll : 회원 정보 조회 성공")
+    public void selectAll() throws Exception {
+        // given
+        UserEntity entity1 = new UserEntity(1L, "password", "nickname", "name", "010-1234-5678", "1@naver.com", LocalDateTime.now());
+        UserEntity entity2 = new UserEntity(2L, "password", "nickname", "name", "010-1234-5678", "2@naver.com", LocalDateTime.now());
+
+        // when
+        userRepository.save(entity1);
+        userRepository.save(entity2);
+        List<UserEntity> result = userRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+
     }
 }
